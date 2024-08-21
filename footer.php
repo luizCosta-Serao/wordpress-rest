282771bb7910e28920f4031a3487cde524a97daf
Luiz Serão <luiz.serao@hotmail.com>
2024-08-21 08:46:08 -0300

    template das páginas

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <footer>
 			<nav class="nav-footer">
 				<ul>
-					<li class="current_page_item"><a href="index.html">Menu</a></li>
-					<li><a href="sobre.html">Sobre</a></li>
-					<li><a href="contato.html">Contato</a></li>
+					<li class="current_page_item"><a href="/wp-teste/">Menu</a></li>
+					<li><a href="/wp-teste/sobre">Sobre</a></li>
+					<li><a href="/wp-teste/contato">Contato</a></li>
 				</ul>
 			</nav>
 
